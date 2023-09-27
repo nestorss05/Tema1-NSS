c5dcae494a73c015d0872cd84b5e12d2ead24617
nsanchez <nsanchez@106-27.info2>
2023-09-27 08:55:04 +0200

    Ejercicio 1 Parte 2 REV

--- src/ejercicio1p2/Ejercicio1P2.java
+++ src/ejercicio1p2/Ejercicio1P2.java
@@ -3,21 +3,24 @@ import java.util.Scanner;
 public class Ejercicio1P2 {
 
 	public static void main(String[] args) {
-		//Registra las variables doubles kilos, manzanas y peras
-		double kilos, manzanas, peras;
+		//Registra las variables doubles total, manzanas y peras
+		double total, manzanas, peras;
+		final double EUROM, EUROP;
+		//Dale valor a las variables finales, EUROM y EUROP, con sus respectivos pesos
+		EUROM=2.35;
+		EUROP=1.95;
 		//Inicia el Scanner
 		Scanner sc= new Scanner (System.in);
 		/*Pregunta al usuario los kilos de manzanas y peras
 		NOTA: Se multiplicara los kilos por los respectivos €/kilo y se guardaran en las respectivas variables manzanas y peras*/
 		System.out.println("¿Cuantos kilos de manzanas se han vendido?");
-		kilos=sc.nextDouble();
-		manzanas=kilos*2.35;
+		manzanas=sc.nextDouble();
 		System.out.println("¿Cuantos kilos de peras se han vendido?");
-		kilos=sc.nextDouble();
-		peras=kilos*1.95;
-		//Muestra al usuario las ganancias
-		System.out.println("Se ha ganado "+manzanas+"€ con las manzanas");
-		System.out.println("Se ha ganado "+peras+"€ con las peras");
+		peras=sc.nextDouble();
+		//Procede a hacer las operaciones respectivas y guardarlas en la variable total
+		total=(manzanas*EUROM)+(peras*EUROP);
+		//Muestra al usuario las ganancias totales
+		System.out.println("Se ha ganado "+total+"€ entre las manzanas y las peras");
 		//Cierra el Scanner
 		sc.close();
 	}
